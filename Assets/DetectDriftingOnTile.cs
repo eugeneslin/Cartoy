945e8a9b7d6972d17e74236b37dfa26e54ee260b
eugeneslin <eugene_lin@hotmail.com>
2023-03-15 14:20:30 -0700

    Added image and tile prefab

---
+++ Assets/DetectDriftingOnTile.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectDriftingOnTile : MonoBehaviour
+{
+    private bool isOnPanel = false;
+    private bool isDrifting = false;
+    private GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Tile"))
+        {
+            isOnPanel = true;
+            panel = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Tile"))
+        {
+            isOnPanel = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PrometeoCarController prometeoCarController = GetComponentInParent<PrometeoCarController>();
+        isDrifting = prometeoCarController.isDrifting;
+
+        if (isOnPanel && isDrifting)
+        {
+            panel.GetComponent<TileController>().DoDriftDamage();
+        }
+    }
+}
---
+++ Assets/TileController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void DoDriftDamage()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
